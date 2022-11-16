6a6dbb49e93f608f167dce400348621a83697e05
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2022-11-16 14:14:38 -0500

    Implement working on the side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,27 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons';
+import { logo } from '../assets';
+import { links } from '../assets/constants';
+
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuClose]
+
+
+  return (
+
+    <>
+    <div className=''>
+
+    </div>
+    </>
+    );
+
+
+
+
+}
+
 
 export default Sidebar;
