87c9f8b666790f588d1bf422d369694a0ab25495
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2022-11-15 21:11:18 -0500

    Implement Play and Pause functionalities

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,14 +1,20 @@
+import { useDispatch, useSelector} from 'react-redux'
 import { Error, Loader, SongCard} from "../components"
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
 
-    
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
+    
     if(isFetching) return <Loader title="Loading songs..."/>;
     if(error) return <Error />;
+    
     return (
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -27,7 +33,10 @@ const Discover = () => {
                     <SongCard 
                         key={song.key} 
                         song={song}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
                         i={i}
+                        data={data}
                     />
                 ))}
             </div>
