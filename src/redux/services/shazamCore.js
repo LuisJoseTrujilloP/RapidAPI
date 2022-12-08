5cee62ab88a60a41faf2b6ed162d72d7e42b3506
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2022-12-08 10:01:59 -0500

    New commit

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,21 +13,17 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
       getTopCharts: builder.query({ query: () => '/charts/world' }),
-      getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-      getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
-      getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
-      getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
       getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
       getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-    }),
-  });
-  
+      getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+      getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+      getSongByGenders: builder.query({ query: (genders) => `/tracks/by_genders?genders=${genders}` }),
+  })
+})
   export const {
     useGetTopChartsQuery,
-    useGetSongsByGenreQuery,
-    useGetSongsByCountryQuery,
-    useGetSongsBySearchQuery,
     useGetArtistDetailsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
+    useGetSongsByGenreQuery,
   } = shazamCoreApi;
