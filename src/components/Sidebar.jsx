d79439015424da6c0aa239f008d99baf8c29f500
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-13 11:21:55 -0400

    Fixing front end proportions mistake

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,21 +33,19 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <div className="md:flex hidden flex-col w-[200px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
-
-      {/* Mobile sidebar */}
       <div className="absolute md:hidden block top-6 right-3">
-        {!mobileMenuOpen ? (
-          <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
-        ) : (
-          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
-        )}
+        {mobileMenuOpen ? (
+          <RiCloseLine className="w-4 h-4 mr-0.5 text-white" onClick={() => setMobileMenuOpen(false)} />
+        ): 
+          <HiOutlineMenu className="w-4 h-4 mr-0.5 text-white" onClick={() => setMobileMenuOpen(true)} />
+        }
       </div>
 
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div className={`absolute top-0 h-screen w-1/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
