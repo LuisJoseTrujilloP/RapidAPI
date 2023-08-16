369d83518375106a3323fd0456b5ee09ca3de38a
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-16 15:38:00 -0400

    Finished TopCharts, fixing mistake with component's name

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -32,7 +32,7 @@ return (
     <div className='flex flex-col'>
         <h2 className='font-bold text 3x1 text-white text-left mt-4 mb-10'>
             Around You 
-            <span className='font-black'>` {country}`</span>
+            <span className='font-black'> {country}</span>
         </h2>
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
             {data?.map((song, i) => (
--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -5,34 +5,21 @@ import { useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
-const AroundYou = () => {
-const [ country, setCountry ] = useState('');
-const [ loading, setLoading ] = useState(true);
+
+const TopCharts = () => {
 const { activeSong, isPlaying } = useSelector((state) => state.player);
-const { data, isFetching, error } = useGetSongsByCountryQuery(country)
+const { data, isFetching, error } = useGetTopChartsQuery()
 
 
-
-
-useEffect(() => {   
-
-    axios
-    .get(`https://geo.ipify.org/api/v2/country?apiKey=at_cfqkRfHQR9mmB2GmZwMNHymrH3TVD&`)
-    .then((res) => setCountry(res?.data?.location?.country))
-    .catch((err) => console.log(err))
-    .finally(() => setLoading(false))
-}, [country])
-
-if (isFetching && loading) return <Loader title='Loading songs around you' />;
-if (error && country != '') return <Error/>
+if (isFetching && loading) return <Loader title='Loading top charts' />;
+if (error) return <Error/>
 
 
 
 return (
     <div className='flex flex-col'>
         <h2 className='font-bold text 3x1 text-white text-left mt-4 mb-10'>
-            Around You 
-            <span className='font-black'> {country}</span>
+            Discover Top Charts
         </h2>
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
             {data?.map((song, i) => (
@@ -50,4 +37,4 @@ return (
     </div> )
 };
 
-export default AroundYou;
+export default TopCharts;
